626040cdb1301f8a86f1c581a6d566124fbf9834
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-23 21:25:40 +0100

    add: finalizado el widget de navegación horizontal

--- lib/widgets/movie_slider.widget.dart
+++ lib/widgets/movie_slider.widget.dart
@@ -7,8 +7,7 @@ class MovieSliderWidget extends StatelessWidget {
 
     return Container(
       width: double.infinity,
-      height: 250.0,
-      color: Colors.red,
+      height: 260.0,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -17,6 +16,7 @@ class MovieSliderWidget extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Text('Populares', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
           ),
+          SizedBox(height: 5.0),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -37,10 +37,34 @@ class _MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Container(
-      width: 130,
-      height: 190,
-      color: Colors.green,
-      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+      width: 130.0,
+      height: 190.0,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        children: <Widget>[
+
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130.0,
+                height: 190.0,
+                fit: BoxFit.cover
+              ),
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Text(
+            'nelvervkloermvmrelmvlkremlvmrmvmdklmvldfmlvmdflmbv,df,bnm,dfnmbndm,nb,mdnbm,dnfm,nf,bmdnfnb,dnfm,bnd',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center
+          )
+        ],
+      ),
     );
   }
 
