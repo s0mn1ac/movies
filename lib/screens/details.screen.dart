c06f788a714fc21cd74d8779924b65fbd64fd0e7
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-24 11:08:34 +0100

    add: construcción del SliverAppBar para la pantalla details

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -5,11 +5,44 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    // TODO: Actualizar por una instancia de movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('DetailsScreen'),
+      body: CustomScrollView(
+        slivers: [
+
+          _CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200.0,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('movie.title', style: TextStyle(fontSize: 16.0))
         ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),        
       ),
     );
   }
