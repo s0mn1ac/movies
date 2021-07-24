0b64db8949a58cdbcee086dd622136c36476b0b0
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-24 11:21:40 +0100

    add: poster y título de la película

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -11,8 +11,12 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ]),
+          )
         ],
       ),
     );
@@ -43,6 +47,48 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
         ),        
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150.0,
+              // fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('movie.title', style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2),
+              Text('movie.originalTitle', style: textTheme.subtitle1, overflow: TextOverflow.ellipsis, maxLines: 1),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.star_outline, size: 15.0, color: Colors.grey),
+                  SizedBox(width: 5.0),
+                  Text('movie.voteAverage', style: textTheme.caption)
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
