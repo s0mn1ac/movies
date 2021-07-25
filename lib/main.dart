4a921492eb62fbd7fe6b84ef36a6a7d7584ab48b
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-25 12:13:48 +0100

    add: provider y paquete http

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:movies/providers/movies.provider.dart';
 
 import 'screens/screens.dart';
  
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (BuildContext context) => MoviesProvider(), lazy: false)
+      ],
+      child: MyApp(),
+    );
+  }
+}
  
 class MyApp extends StatelessWidget {
 
